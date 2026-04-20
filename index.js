5a26bf7c274579111f3ce42957bd1a7ca231a064
davizin-ops <davirodrigued61@gmail.com>
2026-04-20 00:29:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Servidor rodando 🚀");
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/chat", (req, res) => {
+  const msg = req.body.mensagem;
+
+  let resposta = "Ainda estou aprendendo 😄";
+
+  if (msg.toLowerCase().includes("oi")) {
+    resposta = "Olá! Como posso te ajudar?";
+  }
+
+  res.json({ resposta });
 });
 
 app.listen(PORT, () => {
